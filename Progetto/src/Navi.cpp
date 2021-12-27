3f52390bc487fc5b02a3829b27a03365b6cae997
Bizzzio <fachi2001@gmail.com>
2021-12-27 22:08:49 +0100

    spostato includee

--- Progetto/src/Navi.cpp
+++ Progetto/src/Navi.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
+#include "Navi.h"
 using std::cout;
 using std::endl;
 
-
-#include "Navi.h"
-  
-
-void Navi::SetX(int x){
-  X = x; 
+void Navi::SetX(int x)
+{
+  X = x;
 }
 
-void Navi::SetY(int y){
-  X = x; 
+void Navi::SetY(int y)
+{
+  X = x;
 }
 
-void SetHorizontal (bool horizontal){
-  Horizontal=horizontal;
+void SetHorizontal(bool horizontal)
+{
+  Horizontal = horizontal;
 }
 
-int Navi::GetX() const{
-  return X; 
+int Navi::GetX() const
+{
+  return X;
 }
 
-int Navi::GetY() const{
-  return Y; 
+int Navi::GetY() const
+{
+  return Y;
 }
 
-bool IsHorizontal () const {
+bool IsHorizontal() const
+{
   return Horizontal;
 }
 
-vector<bool> GetVector() const {
+vector<bool> GetVector() const
+{
   return Hit;
 }
