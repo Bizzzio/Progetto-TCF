c98bc6b7df0354b13daba71a668431f0a62b1ea9
Bizzzio <fachi2001@gmail.com>
2021-12-29 16:09:55 +0100

    test con 2 navi

--- Progetto/src/main.cpp
+++ Progetto/src/main.cpp
@@ -11,12 +11,15 @@ using std::vector;
 int main()
 {
 	Griglia griglia(6);
-	Navi2 nave(true, 1, 1);
+	Navi2 nave(false, 1, 1);
 	Navi *nvptr = &nave;
+	Navi2 nave1(true, 3, 4);
+	Navi *nvptr1 = &nave1;
 	griglia.grid[1][1] = nvptr;
 	cout<<"ciao"<<endl;
-	griglia.grid[1][2] = nvptr;
-
+	griglia.grid[2][1] = nvptr;
+	griglia.grid[3][4] = nvptr1;
+	griglia.grid[3][5] = nvptr1;
 	for (int i = 0; i < 6; i++)
 	{
 		for (int j = 0; j < 6; j++)
