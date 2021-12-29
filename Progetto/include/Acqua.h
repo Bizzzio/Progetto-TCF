c15dc327fca919b18952de6125cff563e2f0530b
Bizzzio <fachi2001@gmail.com>
2021-12-29 18:18:01 +0100

    fix

--- Progetto/include/Acqua.h
+++ Progetto/include/Acqua.h
@@ -8,15 +8,14 @@ class Acqua : public Navi{
 public:
   Acqua()
   {
-    Length=0;
-    Horizontal=false;
-    X=0;
-    Y=0;
+    SetLength(0);
+    SetHorizontal(false);
+    SetX(0);
+    SetY(0);
   }
-  Acqua();
-	virtual void DrawEnemy(int, int) const {cout<<"~ "};
-	virtual void DrawAlly(int, int) const {cout <<"~ "};
-	virtual bool Sunk(vector<bool>) const {return false};
+	virtual void DrawEnemy(int, int) const {cout<<"~ ";}
+	virtual void DrawAlly(int, int) const {cout <<"~ ";}
+	virtual bool Sunk(vector<bool>) const {return false;}
 	virtual void Strike(int, int) {};
   
 };
