64738b5f76d96eb752661dd2d347a440819b9b42
Bizzzio <fachi2001@gmail.com>
2021-12-29 13:57:03 +0100

    cambiato il gitignore

--- Progetto/src/main.cpp
+++ Progetto/src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
 
 int main()
 {
 	std::cout << "Ciao! sono arianna" << std::endl;
+	vector<vector<int> > griglia;
+	for (int i = 0; i < 10; i++)
+	{
+		vector<int> temp;
+		temp.push_back(0);
+		for (int j = 0; j < 10; j++)
+		{
+
+			griglia.push_back(temp); //aggiunge una riga
+			griglia[i].push_back(0);
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << griglia[i][j];
+		}
+		cout << endl;
+	}
 }
