fad271a0418622b8a661183a0071825bc6aba54a
Arianna Fassino <fassinoarianna@gmail.com>
2022-01-01 21:18:31 +0100

    factory.h

---
+++ Progetto/include/Factory.h
@@ -0,0 +1,22 @@
+#ifndef FACTORY_H
+#define FACTORY_H
+#include <iostream>
+using namespace std;
+#include <vector>
+#include 'Navi.h'
+#include 'Griglia.h'
+
+class Factory {
+
+	public:
+  Factory(int dim, int n2,int n3, int n4, int n5, int n6);
+	~ Factory();
+  void SetFleet(int n2, int n3, int n4, int n5, int n6);
+  vector<Navi*> GetFleet() const;
+   
+  private:
+  vector<Navi*> fleet;
+  Griglia grid;
+  
+};
+#endif
