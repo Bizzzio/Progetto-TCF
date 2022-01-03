21adbc83140248c06396d135ae3b9f9f149634a6
Bizzzio <fachi2001@gmail.com>
2022-01-03 14:49:10 +0100

    test

--- Progetto/src/Main.cpp
+++ Progetto/src/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <conio.h>
 #include "Factory.h"
 #include "Play.h"
 
@@ -9,6 +10,17 @@ using std::vector;
 
 int main()
 {
+	#ifdef _WIN32
+		cout<<"ciao";
+		getch();
+		system("cls");
+	#endif
+
+	#ifdef __unix__
+		cout<<"ciao";
+		getch();
+		system("cls");
+	#endif
 	Factory* grid1= new Factory(8, 2, 0, 0, 0, 0);
 	Factory* grid2= new Factory(8, 2, 0, 0, 0, 0);
 	Play game(grid1,grid2);
