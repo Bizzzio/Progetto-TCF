28aa4c58f4713d2ecf36d52146fe3ef2a1ee7678
Marcello Di Costanzo <marcellodicostanzo00@gmail.com>
2022-01-04 11:26:12 +0100

    MenuPrincipale

--- Progetto/include/menuprincipale.h
+++ Progetto/include/menuprincipale.h
@@ -4,15 +4,22 @@
 #include "menu.h"
 #include "MenuPlay.h"
 #include "MenuOption.h"
+//#include "MenuInstructions.h"
+//#include "MenuCredits.h"
+
 class MenuPrincipale : public Menu
 {
 public:
+	MenuPrincipale();
+    ~MenuPrincipale();
 	virtual void Draw() const;
+    void SelectWindows() const;
+    void SelectOthers() const;
+    void PrintVoci(int) const;
 
 private:
 	const vector<string> voci{"Play", "Instructions", "Options", "Credits", "Quit"};
-	MenuPlay menuplay;
-	MenuOption menuoption;
+	const vector<Menu*> Menus;
 };
 
 #endif
