5bd1079dd451b3ced69c3bb38f7256b383bc1d36
Bizzzio <fachi2001@gmail.com>
2022-01-13 10:43:54 +0100

    aggiunto commenti

--- Progetto/include/Menu.h
+++ Progetto/include/Menu.h
@@ -21,28 +21,31 @@ using std::endl;
 using std::string;
 using std::vector;
 
-
 class Menu
 {
 
 public:
-	unsigned int Arrows(unsigned int *pos, vector<Menu *>) const;		//permette di muoversi nel menu con le frecce
-	unsigned int Arrows(unsigned int *pos, vector<string>) const;		//fa la stessa cosa di quello sopra ma stampa vettori di stringhe anzichè le voci dei menu
-	virtual void PrintVoci(unsigned int) const = 0;						//stampa le voci dei menu sottostanti
-	virtual void DrawVoci() const {};									//stampa il nome del menu in cui ci troviamo (viene chiamata dal menu sopra)
-	virtual void Draw() const = 0;										//Gestisce il menu (scelta dell'utente, muoversi con le frecce e stampa dei menu)
-	static void Add(int);												//fa il pushback di un valore a setup
-	static void Set(int, int);											//imposta un certo elemento del vettore setup ad un certo valroe
+	unsigned int Arrows(unsigned int *pos, vector<Menu *>) const; //permette di muoversi nel menu con le frecce
+	unsigned int Arrows(unsigned int *pos, vector<string>) const; //fa la stessa cosa di quello sopra ma stampa vettori di stringhe anzichè le voci dei menu
+	virtual void PrintVoci(unsigned int) const = 0;				  //stampa le voci dei menu sottostanti
+	virtual void DrawVoci() const {};							  //stampa il nome del menu in cui ci troviamo (viene chiamata dal menu sopra)
+	virtual void Draw() const = 0;								  //Gestisce il menu (scelta dell'utente, muoversi con le frecce e stampa dei menu)
+	static void Add(int);										  //fa il pushback di un valore a setup
+	static void Set(int, int);									  //imposta un certo elemento del vettore setup ad un certo valroe
 
-	static vector<int> GetSetup();										//restituisce il setup
+	static vector<int> GetSetup(); //restituisce il setup
 
 #ifndef _WIN32
-	string GetColor() const;											//converte il colore impostato in setup in un ANSI escape code per colorare il menu fuori da windows
+	string GetColor() const; //converte il colore impostato in setup in un ANSI escape code per colorare il menu fuori da windows
 #endif
 
 private:
+	//Setup è il vettore che contiene le informazioni sulle opzioni:
+	//-	l'elemento 0 coincide con il colore
+	//		9 blu, 10 verde,11 azzurro,12 rosso,13 viola,14 gialllo
+	//-	l'elemento 1 è la dimensione della griglia
+	//-	gli elementi da 2 in poi solo il numero di navi di lunghezza pari alla posizione nell'array
 	static vector<int> Setup;
-
 };
 
 #endif
--- Progetto/include/MenuOpzioni.h
+++ Progetto/include/MenuOpzioni.h
@@ -9,21 +9,24 @@ using std::vector;
 #include "Play.h"
 #include "Menu.h"
 
+//la classe menuopzioni si occupa di gestire le opzioni
 class MenuOpzioni : public Menu
 {
 public:
-	MenuOpzioni(int,string,string);
+	//il costruttore prende 3 input: un intero, che rappresenta quale opzione andrà a cambiare,
+	//una stringa, che rappresenta il nome dell'opzione che verrà visto dai menu sopra a questo,
+	//e una seconda stringa, che contiene il nome del file di testo in cui sono contenute le varie opzioni
+	MenuOpzioni(int, string, string);
 	virtual void Draw() const;
 	virtual void PrintVoci(unsigned int) const;
 	void DrawVoci() const;
-    void SetOption(int, unsigned int) const;
-    void AddConfig() const;
+	void SetOption(int, unsigned int) const; 					//Setoption si occupa di cambiare il valore di Setup (statico) a seconda della scelta dell'utente
+	void AddConfig() const;										//Addconfig aggiunge un'opzione al file di testo (ad esempio aggiungere una configurazione di una flotta)
 
 private:
-    
-    int NumeroOpzione;
-	string Nome,Filename;
-	mutable vector<string> Lista{"Torna al menu"};
+	int NumeroOpzione;											//contiene quale elemento di Setup (statico) il menu deve cambiare
+	string Nome, Filename;										//Nome è il nome dell'opzione con cui viene vista dai menu sopra; filename è il nome del file in cui sono contenute le varie opzioni
+	mutable vector<string> Lista{"Torna al menu"};				//lista è un vector che contiene le varie opzioni (ad esempio le varie configurazioni delle flotte)	
 };
 
 #endif
