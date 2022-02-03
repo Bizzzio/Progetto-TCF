83737a7e40e8704b487b41ceaba8ac99f604887d
Vittorio Garretto <vittorio.garretto@gmail.com>
2022-02-03 09:08:12 +0100

    sdf

--- Progetto/src/play.cpp
+++ Progetto/src/play.cpp
@@ -41,60 +41,7 @@ Play::Play(Factory *player1, Factory *player2) : grid1(player1->GetSize()), grid
 }
 
 void Play::PlayBattleship()
-{ // serve fuzione per cancellare gli output
-	/*int x, y;
-	do
-	{
-		cout << typeid(Player1).name() << endl;
-		cout << "Premi un tasto per mostrare la griglia";
-		getch();
-		system(CLEAR);
-		cout << "Turno giocatore 1" << endl;
-		grid2.DrawEnemy();
-		do
-		{
-			cout << "\nDimmi le coordinate che vuoi colpire " << endl;
-			cin >> x >> y;
-
-		} while (!Check(x, y, grid2));
-		grid2.Strike(x, y);
-
-		//grid1.DrawAlly();
-		grid2.DrawEnemy();
-		cout << "Premi un tasto per oscurare" << endl;
-		getch();
-		system(CLEAR);
-		cout << "Premi un tasto per mostrare la griglia" << endl;
-		getch();
-		system(CLEAR);
-		cout << "Turno giocatore 2" << endl;
-		grid1.DrawEnemy();
-		do
-		{
-			cout << "\nDimmi le coordinate che vuoi colpire " << endl;
-			cin >> x >> y;
-
-		} while (!Check(x, y, grid1));
-		grid1.Strike(x, y);
-
-		//grid2.DrawAlly();
-		grid1.DrawEnemy();
-		cout << "Premi un tasto per oscurare" << endl;
-		getch();
-		system(CLEAR);
-
-		//system (CLEAR);
-
-	} while (!Player1->EndGame() && !Player2->EndGame());
-	if (Player1->EndGame())
-	{
-		cout << "GIOCATORE 2 HAI VINTOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
-	}
-	if (Player2->EndGame())
-	{
-		cout << "GIOCATORE 1 HAI VINTOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
-	}
-	getch();*/
+{ 
 	do
 	{
 		Player1->Turn(grid2, 1);	// Polimorfismo su Turn
